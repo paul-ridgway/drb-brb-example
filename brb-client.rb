f8588eba3a17f8143fd2fcc97f08e9f31642c585
Paul Ridgway <paul@thefloow.com>
2016-08-08 12:06:42 +0100

    Initial BRB Service

---
+++ brb-client.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/env ruby
+
+require 'brb'
+
+
+port = 5555
+host = 'localhost'
+
+# Connecting to the core server, retrieving its interface object : core
+# We do not want to expose an object, so the first parameter is nil
+core = BrB::Tunnel.create(nil, "brb://#{host}:#{port}", :verbose => true)
+
+# # Calling 10 times an non blocking method on the distant core server
+# 10.times do
+#   core.simple_api_method # Do not wait for response
+# end
+
+# # Calling 10 times again long treatment time distant methods
+# 10.times do
+#   core.simple_long_api_method # Do not wait for response
+# end
+
+# # Calling a blocking method with _block on the distant core server :
+# puts " >> Calling 1s call, and wait for response..."
+# r = core.simple_api_method_block
+# puts " > Api response : #{r}"
+
+# puts " >> Calling long call, and wait for response..."
+# r = core.simple_long_api_method_block
+# puts " > Api long response : #{r}"
+
+## Calling method with a callback block for handling the return value
+puts "1: Request"
+core.simple_api_method do |r|
+  puts "1: Get the callback response : #{r}"
+end
+
+sleep 0.33
+
+puts "2: Request"
+core.simple_api_method do |r|
+  puts "2: Get the callback response : #{r}"
+end
+
+puts " >> Callback method has been called continue .."
+sleep 2
+
+core.stop_service
+
+# Our job is over, close event machine :
+EM.stop
---
+++ brb-server.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+require 'brb'
+
+class ExposedCoreObject
+  
+  def simple_api_method
+    puts "#{Thread.current} > In simple api method, now sleeping"
+    yield if block_given?
+    sleep 1
+    puts "#{Thread.current} > Done sleeping in simple api method, return"
+    return 'OK'
+  end
+  
+  def simple_long_api_method
+    puts "#{Thread.current} > In simple long api method, now sleeping"
+    sleep 10
+    puts "#{Thread.current} > Done sleeping in long api method, return"
+    return 'OK LONG'
+  end
+  
+end
+
+Thread.abort_on_exception = true
+
+port = 5555
+host = 'localhost'
+
+puts " > Starting the core on brb://#{host}:#{port}"
+BrB::Service.start_service(:object => ExposedCoreObject.new, :verbose => true, :host => host, :port => port)
+EM.reactor_thread.join
